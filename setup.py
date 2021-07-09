c672fb41df0dad6b1638d696cd42f89df71657ab
leifadev <49649192+leifadev@users.noreply.github.com>
2021-07-09 15:03:14 -0500

    progress

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 # from sys import platform as _platform
 import subprocess
 import os
+import time
 
 # script compiles scout based on OS from makefile :D
 
 class setup:
     def __init__(self):
+        # Main Vars
         self.which = ""
         self.version = ""
         self.debug = ""
 
         print(os.getcwd())
 
-        print("Starting compile...")
+        print("Starting compile process...")
 
 
         self.moveOn = input("For use of python3, press enter, for python, enter any key: ")
@@ -22,36 +24,34 @@ class setup:
         else:
             self.version = "python"
         print("Your using: " + self.version)
+        time.sleep(0.5)
+        print("Beginning!")
 
+        self.installmodules()
 
     def installmodules(self):
         self.which = (input("Are using 'pip' or 'pip3': "))
-        if "pip" or "pip3" == self.which:
-            print("**Freezing**")
-            os.remove("requirements.txt")
+        print("Your selected: " + self.which)
+        if self.which in ("pip","pip3"):
+            time.sleep(0.5)
+            try:
+                os.remove("requirements.txt")
+            except:
+                pass
+
             print("Removing requirements.txt in current folder directory...")
             print(os.getcwd() + "is the current executing path. If this isn't, maybe look into the code, and/or submit an issue at the github")
+            print("Feezing :)")
             subprocess.run(self.which + " freeze >> requirements.txt", shell=True)
-            print("**Installing**")
+            print("**Installing**\n\n")
             subprocess.run(self.which + " install -r requirements.txt", shell=True)
         else:
-            print("Not a valid option! pip or pip3")
+            print("Not a valid option! Use pip or pip3\nIf you use another version like pip3.9, try pip3, if that doesn't work contact the developer.")
             return
 
 
     def compile(self, debug, icon, name):
-        icons = {
-            Windows: "scout_logo.ico",
-            darwin: "scout_logo.icns",
-            linux: "scout_logo.png",
-        }
+        pass
 
-        print("\nNow choose your compiler options...:")
-        os.getcwd()
-        self.debug = input("Would you like to export as a debug mode (console output)?\n")
-        self.icon = input("Enter in what icon you would like scout to have.\nChoices:\n" + icons + "\nYou can use a custom path to any image as well.")
-
-        if self.debug == "yes":
-            self.debug = "--windowed"
-        subprocess.run("pyinstaller --" + self.debug, shell=True)
-        print("Done :D")
+if __name__ == '__main__':
+    setup()
