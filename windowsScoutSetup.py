23fe8ecfa5a88a706f34f56d953ab572d3d41acf
leifadev <49649192+leifadev@users.noreply.github.com>
2021-12-18 23:52:36 -0600

    add legacy support installer for windows FFmpeg

---
+++ windowsScoutSetup.py
@@ -0,0 +1,18 @@
+"""
+Script Version: v0.1
+Author: leifadev
+
+This script applies too all versions of Scout v1.5.1 and under,
+to automatically install FFmpeg for Scout AND the system!
+
+This script installs FFmpeg and directs the path to: ... <<---------- FILL IN HERE
+This script will be compiled and shipped out directly as such.
+
+
+NOTE: Make an options available local path and system-wide path
+
+"""
+
+import tkiner as tk
+from tkinter import *
+import webbrowser, getpass
