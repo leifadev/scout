ce949a2206edbff3006926f7754c34ec586549aa
leifadev <49649192+leifadev@users.noreply.github.com>
2021-12-19 22:22:25 -0600

    fixed 404 http error with exception

--- windowsScoutSetup.py
+++ windowsScoutSetup.py
@@ -13,6 +13,50 @@ NOTE: Make an options available local path and system-wide path
 
 """
 
-import tkiner as tk
+import tkinter as tk
 from tkinter import *
-import webbrowser, getpass
+import webbrowser, getpass, wget, sys
+
+
+class Window:
+    def __init__(self, parent):
+
+        self.localPath = ""
+        self.systemPath = ""
+        self.tempDir = "C://" # ADD TEMP DIR FROM WINDOWS
+        self.icon = ""
+
+        parent.title("Scout Windows FFmpeg Installer")
+
+        width=450
+        height=350
+        screenwidth = parent.winfo_screenwidth()
+        screenheight = parent.winfo_screenheight()
+
+        alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+        parent.geometry(alignstr)
+        parent.resizable(width=False, height=False)
+
+        print("Attemping logo downloading...")
+        url = "https://raw.githubusercontent.com/leifadev/scout/main/images/scout_logo_windows_installer.png"
+
+        # Download icon for use if not present
+        if not os.path.isfile(self.tempDir + "scout_logo.png"):
+            wget.download(url, self.tempDir + "scout_windows_installer.png")
+        self.icon = PhotoImage(file=self.tempDir + "scout_windows_installer.png")
+        parent.tk.call('wm', 'iconphoto', parent._w, self.icon)
+
+        parent.update()   # Updates window at startup to be interactive and lifted
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    parent = Tk()
+    app = Window(parent)
+    parent.mainloop()
